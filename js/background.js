d49f190b458e043307617770839b1828cb1b9c82
JmAbuDabi <dambldor91@gmail.com>
2025-08-18 19:05:01 +0300

    background.js ES Modules

--- js/background.js
+++ js/background.js
@@ -1,10 +1,10 @@
-importScripts('yoyo.js', 'common.js', 'jquery.js', 'pako.js', 'scriptsafe.js');
+const api = {}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const { method, args } = message;
-    if (typeof self[method] === 'function') {
+    if (typeof api[method] === 'function') {
         try {
-            const result = self[method](...args);
+            const result = api[method](...args);
             if (result instanceof Promise) {
                 result.then(res => sendResponse({ result: res })).catch(err => sendResponse({ error: err.message }));
             } else {
