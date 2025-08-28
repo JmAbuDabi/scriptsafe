c6cb0311e8cdf7bec083fabf95f712168fe105da
JmAbuDabi <dambldor91@gmail.com>
2025-08-28 04:02:05 +0300

    updated.js ES Module

--- js/updated.js
+++ js/updated.js
@@ -1,9 +1,9 @@
 // ScriptSafe - Copyright (C) andryou
 // Distributed under the terms of the GNU General Public License
 // The GNU General Public License can be found in the gpl.txt file. Alternatively, see <http://www.gnu.org/licenses/>.
+import { version, localStore, getBackgroundPage } from "./common.js";
 var bkg = getBackgroundPage();
 document.addEventListener('DOMContentLoaded', async function () {
-	var version = '2.0.0.0';
 	$("#title").html("ScriptSafe v" + version);
 	$('#versionno').html(version);
 	$("#loadoptionspage").click(function () { location.href = 'options.html'; });
@@ -17,27 +17,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 });
 async function disableNotification() {
 	if (confirm(await bkg.getLocale("updatedisable"))) {
-		localStorage['updatenotify'] = 'false';
+		await localStore.setItem('updatenotify', 'false');
 		$('#message').html(await bkg.getLocale("updatedisablemessage")).stop().fadeIn("slow").delay(2000).fadeOut("slow");
 	}
-}
-
-function getBackgroundPage() {
-	return new Proxy({}, {
-		get(_, method) {
-			return (...args) => {
-				return new Promise((resolve, reject) => {
-					chrome.runtime.sendMessage({ method, args }, (response) => {
-						if (chrome.runtime.lastError) {
-							return reject(new Error(chrome.runtime.lastError.message));
-						}
-						if (response?.error) {
-							return reject(new Error(response.error));
-						}
-						resolve(response?.result);
-					});
-				});
-			};
-		}
-	});
 }
